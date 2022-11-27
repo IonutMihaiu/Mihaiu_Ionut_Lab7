daa38e6e5590d1969f43d1bb77e6af907a27171d
IonutMihaiu <ionutmihaiu4@gmail.com>
2022-11-27 23:05:53 +0200

    Add project files.

---
+++ Mihaiu_Ionut_Lab7/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Mihaiu_Ionut_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Mihaiu_Ionut_Lab7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mihaiu_Ionut_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
